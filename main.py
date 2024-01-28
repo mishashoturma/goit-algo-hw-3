8091bd2f29a850de2e0747b871fb87e9a8150d9a
misha <shoturmamyhajlo@gmail.com>
2024-01-28 19:10:10 +0200

    Завдання 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 from datetime import datetime
 def get_days_from_today(date):
     try:
@@ -11,3 +12,18 @@ def get_days_from_today(date):
 
 date = input("Введіть дату у форматі YYYY-MM-DD: ")  
 get_days_from_today(date)
+'''
+
+
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    for num in [min, max, quantity]:
+        if not (1<= num <= 1000):
+            return []
+        random_numbers = random.sample(range(min, max+1), quantity)
+        return sorted(random_numbers)
+        
+            
+lottery_numbers = get_numbers_ticket(1, 70, 6)
+print(lottery_numbers)
